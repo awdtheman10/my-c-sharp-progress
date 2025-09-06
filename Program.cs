d608e599f35b8feef9dd588cf24f697618d06ac8
Elisio Rey <rey062613@gmail.com>
2025-09-06 15:28:50 -0600

    day 1

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Utils; // my utilities function
+/*
+so up above are just stuff to make things easier i tink
+but thats cool so im happi
+ */
+// i mostly know lua so i want to make coding easier
+// this is written day 1, uploaded 3:25 pm mdt
+// 9/6/25
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string Username = null;
+            string Password = null;
+            bool signedIn = false;
+            string exePath = Assembly.GetExecutingAssembly().Location;
+
+            Shortcuts.print("Hello!");
+
+            string accountsFolder = Path.Combine(Path.GetDirectoryName(exePath), "Accounts");
+            string configFile = Path.Combine(accountsFolder, "config.txt");
+            if (!Directory.Exists(accountsFolder))
+            {
+                Directory.CreateDirectory(accountsFolder);
+            }
+            if (Directory.Exists(accountsFolder) && File.Exists(configFile))
+            {
+                string filedata = File.ReadAllText(configFile);
+                if (filedata.Contains("signedIn = True")) // checks if you ever signed in
+                {
+                    signedIn = true;
+                }
+            }
+            if (!signedIn)
+            {
+                Shortcuts.print("Enter Username:");
+                Username = (string)Shortcuts.Readln();
+                Shortcuts.print("Enter Password:");
+                Password = (string)Shortcuts.Readln();
+                Shortcuts.WriteConfig(configFile, Username, Password, true);
+            } else
+            {
+                Shortcuts.print("You are signed in.");
+                Shortcuts.print("Sign out?");
+                string Data = (string)Shortcuts.Readln();
+                if (Data == "Yes")
+                {
+                    
+                    signedIn = false;
+                    Shortcuts.WriteConfig(configFile, Username, Password, false);
+                }
+            }
+        }
+    }
+}
---
+++ Utils/Shortcuts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+// 9/6/25
+// 3:16 PM MDT Last updated
+namespace Utils
+{
+    internal class Shortcuts
+    {
+        public static void print(string message) {
+            Console.WriteLine(message); // right now this is basic, but it works for now
+        }
+        public static Object SetValue(Object Value, Object newValue)
+        {
+            return newValue;
+        }
+        public static Object GetValue(Object thingy) {
+            return thingy; 
+        }
+        public static Object Readln()
+        {
+            string Line = Console.ReadLine();
+            return Line;
+        }
+        public static System.IO.FileInfo CreateFile(string path, string data, string name) // this is better to use when creating the file than writefile, as they do the same thing, but this can have a name.
+        {
+            System.IO.File.WriteAllText((path + name), data);
+            return new System.IO.FileInfo(path);
+        } // this js makes a file
+        public static void DeleteFile(string path)
+        {
+            System.IO.File.Delete(path); // deletes the file
+        }
+        public static void WriteFile(string path, string data)
+        {
+            System.IO.File.WriteAllText(path, data); // can be used instead of create file, but createfile is better
+        }
+        public static void WriteConfig(string path, string Username, string Password, bool SignedIn)
+        {
+            string FileData = "{" +
+                        " signedIn = " + SignedIn +
+                        " username: " + Username +
+                        " password: " + Password +
+                        "}";
+            WriteFile(path, FileData);
+        }
+    }
+}
