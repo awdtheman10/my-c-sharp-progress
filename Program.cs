42772590e752d08872d73b6ef23750b841631b2c
Elisio Rey <rey062613@gmail.com>
2025-09-16 00:14:42 -0600

    week 2

--- Program.cs
+++ Program.cs
@@ -1,66 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
-using Utils; // my utilities function
-/*
-so up above are just stuff to make things easier i tink
-but thats cool so im happi
- */
-// i mostly know lua so i want to make coding easier
-// this is written day 1, uploaded 3:25 pm mdt
-// 9/6/25
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+using Utils;
+
+// 9/16/25
+// Last updated: 12:06 AM
+
 namespace Main
 {
     internal class Program
     {
-
+        static void FirstMethod()
+        {
+            Console.WriteLine("First ever method i've wrote"); // almost forgot the ; here ._.
+        }
+        class uhh
+        {
+        uhh myObj = new uhh(); // i do not fucking understand this. im a roblox lua dev not a C# dev (but i should learn it)
+        }
         static void Main(string[] args)
         {
-            string Username = null;
-            string Password = null;
-            bool signedIn = false;
-            string exePath = Assembly.GetExecutingAssembly().Location;
+            string Username = null; // i KNOW WHAT NULL IS! YEAAAA
+            string Password = null; // null.
+            bool signedIn = false; // you are not signed in.
+            string exePath = Assembly.GetExecutingAssembly().Location; // idk why i called it an exe its a .cs lol
 
-            Shortcuts.print("Hello!");
+            Shortcuts.print("Hello!"); // hi
+            FirstMethod(); // i figured tis out
 
-            string accountsFolder = Path.Combine(Path.GetDirectoryName(exePath), "Accounts");
-            string configFile = Path.Combine(accountsFolder, "config.txt");
-            if (!Directory.Exists(accountsFolder))
+            string accountsFolder = Path.Combine(Path.GetDirectoryName(exePath), "Accounts"); // im not sure how this is secure
+            string configFile = Path.Combine(accountsFolder, "config.txt"); // took a while as well
+
+            if (!Directory.Exists(accountsFolder)) // this took an hour to figure out on my own
             {
-                Directory.CreateDirectory(accountsFolder);
+                Directory.CreateDirectory(accountsFolder); // if no accounts then make accounts
             }
-            if (Directory.Exists(accountsFolder) && File.Exists(configFile))
+
+            if (File.Exists(configFile))
             {
                 string filedata = File.ReadAllText(configFile);
                 if (filedata.Contains("signedIn = True")) // checks if you ever signed in
                 {
-                    signedIn = true;
+                    signedIn = true; // you are signed in.
                 }
             }
-            if (!signedIn)
+
+            if (!signedIn) // i learned how to use ! from just other code lol, its funny how i learn
             {
-                Shortcuts.print("Enter Username:");
+                Shortcuts.print("Enter Username:"); // you should totally put "skibiditoilet696942067tralalerotralala"
                 Username = (string)Shortcuts.Readln();
-                Shortcuts.print("Enter Password:");
+                Shortcuts.print("Enter Password:"); // put 1234, i dare you
                 Password = (string)Shortcuts.Readln();
-                Shortcuts.WriteConfig(configFile, Username, Password, true);
-            } else
+                Shortcuts.WriteConfig(configFile, Username, Password, true); // i didnt wan write over and over
+            }
+            else
             {
-                Shortcuts.print("You are signed in.");
-                Shortcuts.print("Sign out?");
+                Shortcuts.print("You are signed in."); // good boy
+
+                Shortcuts.print("Sign out?"); // dont.
                 string Data = (string)Shortcuts.Readln();
-                if (Data == "Yes")
+                if (Data.ToLower() == "yes") // YOU MONKEY
                 {
-                    
-                    signedIn = false;
-                    Shortcuts.WriteConfig(configFile, Username, Password, false);
+                    signedIn = false; // you are not signed in.
+                    Shortcuts.WriteConfig(configFile, Username, Password, false); // again
                 }
             }
         }
     }
 }
+// this app is mostly just a test app. dont take my comments seriously
--- Utils/Shortcuts.cs
+++ Utils/Shortcuts.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-// 9/6/25
-// 3:16 PM MDT Last updated
+
+// 9/16/25
+// Last updated: 12:12 AM
+
+
+
 namespace Utils
 {
     internal class Shortcuts
@@ -14,14 +18,14 @@ namespace Utils
         public static void print(string message) {
             Console.WriteLine(message); // right now this is basic, but it works for now
         }
-        public static Object SetValue(Object Value, Object newValue)
+        public static Object SetValue(Object Value, Object newValue) // why did i make this
         {
-            return newValue;
+            return newValue; 
         }
-        public static Object GetValue(Object thingy) {
+        public static Object GetValue(Object thingy) { // again, why DID I MAKE THIS??
             return thingy; 
         }
-        public static Object Readln()
+        public static Object Readln() // this is stupid
         {
             string Line = Console.ReadLine();
             return Line;
@@ -29,13 +33,13 @@ namespace Utils
         public static System.IO.FileInfo CreateFile(string path, string data, string name) // this is better to use when creating the file than writefile, as they do the same thing, but this can have a name.
         {
             System.IO.File.WriteAllText((path + name), data);
-            return new System.IO.FileInfo(path);
+            return new System.IO.FileInfo(path + name);
         } // this js makes a file
-        public static void DeleteFile(string path)
+        public static void DeleteFile(string path) // why is this even here
         {
             System.IO.File.Delete(path); // deletes the file
         }
-        public static void WriteFile(string path, string data)
+        public static void WriteFile(string path, string data) // dont use this unless you want to WRITE TO A FILE. 
         {
             System.IO.File.WriteAllText(path, data); // can be used instead of create file, but createfile is better
         }
@@ -45,8 +49,13 @@ namespace Utils
                         " signedIn = " + SignedIn +
                         " username: " + Username +
                         " password: " + Password +
-                        "}";
+                        "}"; // so basic ik ik. no IT IS NOT JSON. IT IS SIMPLY MY OWN FORMAT I WANT (but its basically json so WHO CARES)
             WriteFile(path, FileData);
-        }
+        } // writes the config needed
+        public static void PlaySound(int frequency, int duration) // WHY??? WHY DID I MAKE THIS
+        {
+            Console.Beep(frequency, duration);
+        } // idk why this is here, but it is lol
+
     }
 }
